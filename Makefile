c364f87b1d1497915ecbed0353b742c004fc14e5
Thiago Beppe <thiagoabeppe@gmail.com>
2023-01-27 15:05:34 -0300

    finishing day_4

---
+++ Makefile
@@ -0,0 +1,8 @@
+install:
+	pip install -e .
+
+install_dev:
+	pip install -e '.[dev]'
+
+install_test:
+	pip install -e '.[test]'
--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+import os
 from setuptools import setup
+
+def read(*paths):
+    rootpath = os.path.dirname(__file__)
+    filepath = os.path.join(rootpath, *paths)
+    with open(filepath) as file_:
+        return file_.read().strip()
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(("#","-","git+", '"'))
+    ]
 
 setup(
     name = "dundie",
@@ -11,5 +25,10 @@ setup(
             "dundie = dundie.__main__:main"
         ],
 
+    },
+    install_requires = read_requirements("requirements.txt"),
+    extras_require={
+        "test": read_requirements("requirements.dev.txt"),
+        "dev":  read_requirements("requirements.test.txt")
     }
 )
